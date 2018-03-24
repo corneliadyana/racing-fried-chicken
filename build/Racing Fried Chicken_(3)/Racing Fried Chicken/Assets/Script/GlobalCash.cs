5f7f2eb4ebce57e1367fd85a1650245f31ffabd7
trashynucleus <norikita@windowslive.com>
2018-03-24 12:04:18 +0700

    makasih coco
    heart emoji

---
+++ build/Racing Fried Chicken_(3)/Racing Fried Chicken/Assets/Script/GlobalCash.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalCash : MonoBehaviour {
+
+    /*public int CashValue;
+    public static int TotalCash;
+    public GameObject CashDisplay;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        CashValue = TotalCash;
+        CashDisplay.GetComponent<Text>().text = "Cash $" + CashValue;
+    }
+}
+
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditFinish : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(CreditEnd());
+    }
+
+    IEnumerator CreditEnd()
+    {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene(0);
+    }
+
+    */
+}
