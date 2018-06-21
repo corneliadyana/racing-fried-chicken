a41a521e5f5149a93cbb47a0a96305b92440d52f
imbeben <36806784+imbeben@users.noreply.github.com>
2018-06-22 00:24:40 +0700

    Update_WOAsset_1
    update terbaru tanpa asset
    karena upate di Lappy gasanggup buat render
    - Mini Map
    - Track Selection
    - Main Menu
    - SkyBox
    - New Track (total 5 tapi 4 masih polos)

--- build/Assets/Script/ButtonOption.cs
+++ build/Assets/Script/ButtonOption.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonOption : MonoBehaviour {
 
-    /*
+    
     public void PlayGame()
     {
         SceneManager.LoadScene(2);
@@ -26,5 +27,25 @@ public class ButtonOption : MonoBehaviour {
     {
         SceneManager.LoadScene(2);
     }
-    */
+
+    public void Track02()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Track03()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Track04()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Track05()
+    {
+        SceneManager.LoadScene(6);
+    }
+
 }
