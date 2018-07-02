0f4ec39a43cffcfe9f2f17d706a538778e2475c6
Bento Putra Hermanto <elben9229@gmail.com>
2018-07-02 13:18:12 +0700

    DeleteFileLmaKarenaBUG

--- Racing Fried Chicken 'UNITY'/Assets/Script/CarControlActive.js
+++
@@ -1,7 +0,0 @@
-var CarControl : GameObject;
-var Dreamcar01 : GameObject;
-
-function Start () {
-	CarControl.GetComponent("CarController").enabled = true;
-	Dreamcar01.GetComponent("CarAIControl").enabled = true;
-}
